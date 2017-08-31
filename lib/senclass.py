59f889a5e89cd0cb942ded4fa2cd779fd557fc98
Nicole Stevens <nicci@ducksfeet.com>
2017-08-31 10:06:22 -0700

    Updated comments for senClass to proper terminology

--- lib/senclass.py
+++ lib/senclass.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('.')
 import dht
 import ds3232
-''' A nice little wrapper to instantiate sensor of proper class based on stype. '''
+''' A nice little wrapper providing object reflection for the (currently) two defined sensor types. '''
 
 def instance(stype, rtype, sendef):
     return globals()[stype].Sensor(rtype,sendef)
